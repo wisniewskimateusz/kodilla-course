2dd2da5b50e8802f780b75ed86e38a152d36b2f6
Mateusz <mateusz@com>
2020-10-28 20:36:35 +0100

    7.2 Creating immutable FromUser class.

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.reference.FunctionalCalculator;
@@ -34,5 +35,9 @@ public class StreamMain {
         poemBeautifier.print();
         poemBeautifier.beautify("abc", String::toLowerCase);
         poemBeautifier.print();
+
+        //Stream
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
