b2f517e0fc5743831e51f380a56da4ec42a2a4f9
Mateusz <mateusz@com>
2020-10-29 11:49:15 +0100

    7.3 Refactor filter by year of birth.

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,15 +3,18 @@ package com.kodilla.stream;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
         Forum forum = new Forum();
+        int actualYear = LocalDate.now().getYear();
+
         Map<Integer, ForumUser> theResultMapOfForum = forum.getUserList().stream()
                 .filter(user -> user.getSex() == 'M')
-                .filter(user -> user.getDateOfBirth().getYear() <= 2000)
+                .filter(user -> (actualYear - user.getDateOfBirth().getYear()) >= 20)
                 .filter(user -> user.getPostQuantity() >= 1)
                 .collect(Collectors.toMap(ForumUser::getId, user -> user));
 
