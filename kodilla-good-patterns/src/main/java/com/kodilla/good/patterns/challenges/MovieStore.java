719a01f8b7901b6185cc1a2699be5318e61c81f5
Mateusz <mateusz@com>
2021-01-04 17:32:57 +0100

    7.7 Rock, paper scissors game.

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -31,7 +31,6 @@ public class MovieStore {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
 
-        System.out.println("\n------------------");
         String movies = movieStore.getMovies()
                 .entrySet()
                 .stream()
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderService.java
@@ -1,4 +1,16 @@
 package com.kodilla.good.patterns.challenges;
 
 public class ProductOrderService {
+    //    private RentalService rentalService;
+//    private OrderRepository orderRepository;
+//
+//    public RentalProcessor(final RentalService rentalService, final RentalRepository rentalRepository) {
+//        this.informationService = informationService;
+//        this.rentalService = rentalService;
+//        this.rentalRepository = rentalRepository;
+//    }
+//
+//    public OrderDto process(final RentRequest rentRequest) {
+//
+//    }
 }
