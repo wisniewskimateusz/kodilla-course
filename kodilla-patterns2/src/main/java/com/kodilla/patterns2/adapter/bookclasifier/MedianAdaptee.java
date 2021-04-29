4c9c85c4d281562c379f468fccccd6c60c1cb741
Mateusz <mateusz@com>
2021-04-29 21:13:50 +0200

    24.5 Adapter pattern - bookclasifier fix

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdaptee.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdaptee.java
@@ -4,19 +4,21 @@ import com.kodilla.patterns2.adapter.bookclasifier.libraryb.Bookb;
 import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
 import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookStatistics;
 import com.kodilla.patterns2.adapter.bookclasifier.libraryb.Statistics;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Map;
 
 public class MedianAdaptee implements BookStatistics {
+
+    Statistics statistics = new Statistics();
+
     @Override
     public int averagePublicationYear(Map<BookSignature, Bookb> books) {
-        Statistics statistics = new Statistics();
         return statistics.averagePublicationYear(books);
     }
 
     @Override
     public int medianPublicationYear(Map<BookSignature, Bookb> books) {
-        Statistics statistics = new Statistics();
         return statistics.averagePublicationYear(books);
     }
 }
